b4df78584fa1e2f1c608ae0013ca7fb0621c7b6d
dawwson <shines1427@gmail.com>
2023-08-24 22:33:47 +0900

    Feat: 추가한 할 일 보여주기 기능 추가

--- src/component/TodoList.js
+++ src/component/TodoList.js
@@ -1,15 +1,15 @@
 import TodoItem from "./TodoItem";
 import "./TodoList.css";
 
-const TodoList = () => {
+const TodoList = ({ items }) => {
   return (
     <div className="TodoList">
-      <h4>Todo List</h4>
+      <h4>Todo List 💬</h4>
       <input className="searchbar" placeholder="검색어를 입력하세요" />
       <div className="list_wrapper">
-        <TodoItem />
-        <TodoItem />
-        <TodoItem />
+        {items.map((item) => (
+          <TodoItem key={item.id} {...item} />
+        ))}
       </div>
     </div>
   );
