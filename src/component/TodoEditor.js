a39e335bff0400ed99acc754bc0703c619cbaf53
dawwson <shines1427@gmail.com>
2023-08-24 21:42:10 +0900

    Refactor: 빈 입력값 방지

--- src/component/TodoEditor.js
+++ src/component/TodoEditor.js
@@ -1,16 +1,21 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import "./TodoEditor.css";
 
 const TodoEditor = (props) => {
   const { onCreate } = props;
 
   const [content, setContent] = useState("");
+  const inputRef = useRef();
 
   const handleOnChangeInput = (e) => {
     setContent(e.target.value);
   };
 
   const handleOnClickButton = () => {
+    if (!content) {
+      // 빈 입력값 방지 => 입력창에 포커스 주기
+      return inputRef.current.focus();
+    }
     onCreate(content);
   };
 
@@ -19,6 +24,7 @@ const TodoEditor = (props) => {
       <h4>새로운 Todo 작성하기 ✏️</h4>
       <div className="editor_wrapper">
         <input
+          ref={inputRef}
           placeholder="새로운 Todo..."
           value={content}
           onChange={handleOnChangeInput}
