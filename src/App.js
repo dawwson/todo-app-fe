87e09cee734a86f9eecc7b7fe50210f288198910
dawwson <shines1427@gmail.com>
2023-08-24 21:10:25 +0900

    feat: mock 데이터로 state 설정

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
+import { useState } from "react";
 import "./App.css";
 import Header from "./component/Header";
 import TodoEditor from "./component/TodoEditor";
 import TodoList from "./component/TodoList";
 
+const mockData = [
+  {
+    id: 1,
+    isDone: false,
+    content: "포트폴리오 만들기",
+    createdAt: new Date().getTime(),
+  },
+  {
+    id: 2,
+    isDone: false,
+    content: "블로그 글 쓰기",
+    createdAt: new Date().getTime(),
+  },
+  {
+    id: 3,
+    isDone: false,
+    content: "Spring 강의 듣기",
+    createdAt: new Date().getTime(),
+  },
+];
+
 function App() {
+  const [todo, setTodo] = useState(mockData);
+
   return (
     <div className="App">
       <Header />
