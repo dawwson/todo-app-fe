243a838d771f549192c135cbe69419e84f68f470
dawwson <shines1427@gmail.com>
2023-08-25 21:09:52 +0900

    Feat: 검색 기능 추가

--- src/component/TodoList.js
+++ src/component/TodoList.js
@@ -1,13 +1,31 @@
+import { useState } from "react";
 import TodoItem from "./TodoItem";
 import "./TodoList.css";
 
 const TodoList = ({ items }) => {
+  const [search, setSearch] = useState("");
+
+  const handleOnChangeSearch = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const getSearchResult = () => {
+    return search === ""
+      ? items
+      : items.filter((item) => item.content.includes(search));
+  };
+
   return (
     <div className="TodoList">
       <h4>Todo List 💬</h4>
-      <input className="searchbar" placeholder="검색어를 입력하세요" />
+      <input
+        className="searchbar"
+        placeholder="검색어를 입력하세요"
+        value={search}
+        onChange={handleOnChangeSearch}
+      />
       <div className="list_wrapper">
-        {items.map((item) => (
+        {getSearchResult().map((item) => (
           <TodoItem key={item.id} {...item} />
         ))}
       </div>
