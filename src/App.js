2bab480df8eca4ccfc6100a3c081836c8960f683
dawwson <shines1427@gmail.com>
2023-08-26 22:39:12 +0900

    Feat: Mock 서버 연동

--- src/App.js
+++ src/App.js
@@ -1,60 +1,69 @@
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./App.css";
 import Header from "./component/Header";
 import TodoEditor from "./component/TodoEditor";
 import TodoList from "./component/TodoList";
 
-const mockData = [
-  {
-    id: 1,
-    isDone: false,
-    content: "포트폴리오 만들기",
-    createdAt: new Date().getTime(),
-  },
-  {
-    id: 2,
-    isDone: false,
-    content: "블로그 글 쓰기",
-    createdAt: new Date().getTime(),
-  },
-  {
-    id: 3,
-    isDone: false,
-    content: "Spring 강의 듣기",
-    createdAt: new Date().getTime(),
-  },
-];
-
+// TODO: 상태 코드에 따른 예외처리
 function App() {
-  const [todos, setTodos] = useState(mockData);
+  const [todos, setTodos] = useState([]);
   const idRef = useRef(4);
 
-  const handleOnCreate = (content) => {
-    const newItem = {
+  console.log(todos);
+
+  const fetchData = async () => {
+    const res = await fetch("http://localhost:4000/todos");
+    const jsonData = await res.json();
+    setTodos(jsonData);
+  };
+
+  const handleOnCreate = async (content) => {
+    const newTodo = {
       id: idRef.current,
       isDone: false,
       content,
       createdAt: new Date().getTime(),
     };
-
-    setTodos([...todos, newItem]);
+    // setTodos([...todos, newTodo]);
+    await fetch("http://localhost:4000/todos", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(newTodo),
+    });
+    await fetchData();
     idRef.current += 1;
   };
 
-  const handleOnChangeCheckbox = (targetId) => {
-    const updatedTodo = todos.map((todo) => {
-      if (todo.id === targetId) {
-        return { ...todo, isDone: !todo.isDone };
-      }
-      return todo;
+  const handleOnChangeCheckbox = async (targetId) => {
+    // const updatedTodo = todos.map((todo) => {
+    //   if (todo.id === targetId) {
+    //     return { ...todo, isDone: !todo.isDone };
+    //   }
+    //   return todo;
+    // });
+    // setTodos(updatedTodo);
+
+    const todoToUpdate = todos.find((todo) => todo.id === targetId);
+    await fetch(`http://localhost:4000/todos/${targetId}`, {
+      method: "PATCH",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ isDone: !todoToUpdate.isDone }),
     });
-    setTodos(updatedTodo);
+    await fetchData();
   };
 
-  const handleOnDelete = (targetId) => {
-    const updatedTodo = todos.filter((todo) => todo.id !== targetId);
-    setTodos(updatedTodo);
+  const handleOnDelete = async (targetId) => {
+    // const updatedTodo = todos.filter((todo) => todo.id !== targetId);
+    // setTodos(updatedTodo);
+    await fetch(`http://localhost:4000/todos/${targetId}`, {
+      method: "DELETE",
+    });
+    await fetchData();
   };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   return (
     <div className="App">
